a186e069110eb88d221a35c8d132c7700d798c7b
Felipe Velez <fevelezf@unal.edu.co>
2025-11-15 12:01:53 -0500

    se crea la funcion write_cout_words

--- homework/tests/test_migracion.py
+++ homework/tests/test_migracion.py
@@ -1,20 +1,21 @@
 import os
 
+from ..src.wordcount import main
+
 
 def test_migracion():
 
-    if not os.path.exists("data/input/"):
-        raise FileNotFoundError(
-            "El archivo results.tsv no existe. Por favor, asegúrate de ejecutar el script wordcount.py para generar los datos necesarios."
-        )
+    main()
+
+    if not os.path.exists("data/output/results.tsv"):
+        raise FileNotFoundError("El archivo results.tsv no existe.")
 
     results = {}
-
     with open("data/output/results.tsv", "r", encoding="utf-8") as f:
         lines = f.readlines()
     for line in lines:
         key, value = line.strip().split("\t")
-        results[key] = int(value)
+        results[key] = value
 
-    assert results.get("computational", 0) == 3
-    assert results.get("analytics", 0) == 5
+    assert results.get("computational", 0) == "3"
+    assert results.get("analytics", 0) == "5"
