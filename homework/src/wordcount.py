b96df1af785be1c00c00a97cfe48581307150f83
Felipe Velez <fevelezf@unal.edu.co>
2025-11-22 08:36:29 -0500

    organizacion de codigo en wordcount.py

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -4,40 +4,31 @@ import os
 from homework.src._internals.write_count_words import write_count_words
 
 
-def read_all_lines():
+def main():
+    ### read all lines
+    # all_lines = read_all_lines()
     all_lines = []
     input_files_list = os.listdir("data/input/")
 
     for filename in input_files_list:
-        with open(filename, "r", encoding="utf-8") as f:
+        file_path = os.path.join("data/input/", filename)
+        with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
             all_lines.extend(lines)
-    return all_lines
 
-
-def main():
-    ###
-    # all_lines = read_all_lines()
-    input_files_list = os.listdir("data/input/")
-
-    ### read all lines
     ### preprocess lines
+    all_lines = [line.strip().lower() for line in all_lines]
+
     ### split in words
+    words = []
+    for line in all_lines:
+        words.extend(words.strip(", .!?") for words in line.split())
     ### count words
-    ### write count words
-
-    # count the frequency of the words in the files in the input directory
     counter = {}
-    for filename in input_files_list:
-        with open("data/input/" + filename) as f:
-            for l in f:
-                for w in l.split():
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    for word in words:
+        counter[word] = counter.get(word, 0) + 1
 
-    ###
-    # create the directory output/ if it doesn't exist
-
+    ### write count words
     write_count_words(counter)
 
 
